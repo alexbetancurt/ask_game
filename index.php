806f6c148f7c78365f0f4e5f07eafa7c61eeb92e
Alex Betancur <alexbetbu@gmail.com>
2022-05-13 01:00:37 -0500

    Creación de index.php

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1
+>Bienvenido al juego de preguntas y respuestas</h1>
+<p>Este juego consta de 5 preguntas por nivel, cada nivel tiene un premio especial:</p>
+
+<h3>PREMIOS </h3>
+<P>NIVEL 1: 1 MILLON</p>
+<P>NIVEL 2: 5 MILLONES</p>
+<P>NIVEL 3: 20 MILLONES</p>
+<P>NIVEL 4: 50 MILLONES</p>
+<P>NIVEL 5: 100 MILLONES </p>
+
+<h3>Ingresa tu nombre y da click en iniciar para empezar con la primer ronda </h3>
+<p>Si no sabes la respuesta, puedes abandonar el juego para llevarte los premios acomulados. 
+    En caso de errar con una sola pregunta, perderas todo el puntaje</p>
+    <p><strong>Mucha Suerte!</strong></p>
+
+<form action="app.php" method="post">
+    Nombre: <input type="text" name="nombre"><br><br>
+    <input type="submit" value="Iniciar Juego">
+</form>
+
+</body>
+</html>
